1ca6ba75e7a35baf0f625c6ec0aafc84d777d3bf
Suzy Easton <suzy.easton@winedirect.com>
2024-01-30 02:24:44 -0800

    the heck canadians spell colour with a u lol

--- budgetsuzy.sh
+++ budgetsuzy.sh
@@ -5,12 +5,12 @@ FILE="$DIR/budget.csv"
 DATE=$(date +%F) # Current date in YYYY-MM-DD format
 TIME=$(date +%T) # Current time
 
-# Color codes
+# Colour codes
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
+NC='\033[0m' # No Colour
 
 # Function to initialize or reset the CSV file
 initialize_csv() {
