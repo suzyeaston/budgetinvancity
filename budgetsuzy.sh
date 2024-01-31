738c0c53891ca938bec0cc312918ee1f767fdeef
Suzy Easton <suzy.easton@winedirect.com>
2024-01-30 23:35:37 -0800

    simplifying the date comparison by using the YYYY-MM-DD format directly, without converting them to Unix for calculating totals
    of course, because your mom

--- budgetsuzy.sh
+++ budgetsuzy.sh
@@ -88,21 +88,19 @@ delete_all_entries() {
 }
 
 calculate_totals() {
-    start_date=$(date -j -f "%Y-%m-%d" "$1" +%s)
-    end_date=$(date -j -f "%Y-%m-%d" "$2" +%s)
+    start_date="$1"
+    end_date="$2"
 
     total=0
     while IFS=, read -r date time amount category payment_method description recurring recurrence_period due_date
     do
-        # Check if date is in the correct format to avoid conversion errors
         if [[ $date =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
-            date_ts=$(date -j -f "%Y-%m-%d" "$date" +%s)
-            if [[ "$date_ts" -gt "$start_date" && "$date_ts" -le "$end_date" ]]; then
+            if [[ "$date" > "$start_date" && "$date" <= "$end_date" ]]; then
                 total=$(echo "$total + $amount" | bc)
             fi
         fi
     done < "$FILE"
-    echo -e "${GREEN}Total expenses from $(date -j -f "%s" "$start_date" +%F) to $(date -j -f "%s" "$end_date" +%F): $total${NC}"
+    echo -e "${GREEN}Total expenses from $start_date to $end_date: $total${NC}"
 }
 
 # Function to calculate and display real-time totals
